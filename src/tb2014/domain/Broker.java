2569559d35077874ef105d0079ea9d6466660caf
Eugene Husnullin <ehusnullin80@gmail.com>
2014-01-17 12:20:55 +0200

    init commit

---
+++ src/tb2014/domain/Broker.java
@@ -0,0 +1,31 @@
+package tb2014.domain;
+
+public class Broker {
+	private Long id;
+	private String name;
+	private String apiurl;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getApiurl() {
+		return apiurl;
+	}
+
+	public void setApiurl(String apiurl) {
+		this.apiurl = apiurl;
+	}
+}
