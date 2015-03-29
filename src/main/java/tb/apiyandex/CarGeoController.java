1c144b74aaaf0bb07a81665b8d2205ab0aeeb568
echo <goalinternet@gmail.com>
2015-03-29 11:15:07 +0300

    test

--- src/main/java/tb/apiyandex/CarGeoController.java
+++ src/main/java/tb/apiyandex/CarGeoController.java
@@ -8,6 +8,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.xml.parsers.ParserConfigurationException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,6 +27,7 @@ import tb.utils.XmlUtils;
 @Controller("apiyandexCarGeoController")
 @RequestMapping("/cargeo")
 public class CarGeoController {
+	private static final Logger log = LoggerFactory.getLogger(CarGeoController.class);
 
 	@Autowired
 	private CarStateGeoBuilder carStateGeoBuilder;
@@ -35,19 +38,26 @@ public class CarGeoController {
 
 	@RequestMapping(value = "", method = RequestMethod.POST)
 	public void index(HttpServletRequest request, HttpServletResponse response) {
+
+		log.error(request.getParameter("data"));
 		try {
-			Document document = XmlUtils.buildDomDocument(request.getParameter("data"));
-			String brokerClid = carStateGeoBuilder.defineCarStateGeosBrokerClid(document);
-			Broker broker = brokerDao.getByApiId(brokerClid);
-			if (broker != null) {
-				List<CarState> carStates = carStateGeoBuilder.createCarStateGeos(document, new Date());
-				carDao.updateCarStateGeos(broker, carStates);
-				response.setStatus(200);
-			} else {
-				response.setStatus(403);
+			try {
+				Document document = XmlUtils.buildDomDocument(request.getParameter("data"));
+				String brokerClid = carStateGeoBuilder.defineCarStateGeosBrokerClid(document);
+				Broker broker = brokerDao.getByApiId(brokerClid);
+				if (broker != null) {
+					List<CarState> carStates = carStateGeoBuilder.createCarStateGeos(document, new Date());
+					carDao.updateCarStateGeos(broker, carStates);
+					response.setStatus(200);
+				} else {
+					response.setStatus(403);
+				}
+			} catch (ParserConfigurationException | SAXException | IOException e) {
+				response.setStatus(400);
 			}
-		} catch (ParserConfigurationException | SAXException | IOException e) {
-			response.setStatus(400);
+		} catch (Exception e) {
+			log.error("cargeo:", e);
+			throw e;
 		}
 	}
 }
